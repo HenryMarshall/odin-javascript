136fcc9c2175029f01e1b967793280fb29768e3f
hbaughman <hbaughman741@gmail.com>
2014-09-11 16:54:49 -0400

    Refactor into grid object

--- snake/snake.js
+++ snake/snake.js
@@ -1,35 +1,46 @@
 $(document).ready(function() {
 
+var grid = {
+  initialize: function() {
+    var gameboard = this.buildArray(50, 30);
+    var gridHtml = this.buildHtml(gameboard);
+    $('#gameboard').append($(gridHtml));
+  },
 
-function buildArray (x, y) {
-  var grid = [];
-  for (var i = y - 1; i >= 0; i--) {
-    var row = [];
-    for (var j = x - 1; j >= 0; j--) {
-      row.push(' ');
+  buildArray: function(x, y) {
+    var grid = [];
+    for (var i = y - 1; i >= 0; i--) {
+      var row = [];
+      for (var j = x - 1; j >= 0; j--) {
+        row.push(' ');
+      };
+      grid.push(row);
     };
-    grid.push(row);
-  };
-  return grid;
-}
+    return grid;
+  },
 
-function buildHtml (gameboard) {
-  var gridHtml = '';
-  gameboard.forEach(function(row) {
-    var rowHtml = '<div class="row">';
-    row.forEach(function() {
-      rowHtml += '<span class="cell"></span>';
+  buildHtml: function(gameboard) {
+    var gridHtml = '';
+    gameboard.forEach(function(row, rowIdx) {
+      var rowHtml = '<div class="row">';
+      row.forEach(function(cell, cellIdx) {
+        rowHtml += '<span class="cell" id="' + rowIdx + '-' + cellIdx +'"></span>';
+      });
+      rowHtml += '</div>';
+      gridHtml += rowHtml;
     });
-    rowHtml += '</div>';
-    gridHtml += rowHtml;
-  });
-  return gridHtml;
-}
+    return gridHtml;
+  }
+};
+
+var snake = {
+  position: [[20,20]],
+  direction: 'e'
+};
 
 
-var gameboard = buildArray(50, 30);
-var gridHtml = buildHtml(gameboard);
-$('#gameboard').append($(gridHtml));
+grid.initialize();
+
 
 
 // end doc ready
