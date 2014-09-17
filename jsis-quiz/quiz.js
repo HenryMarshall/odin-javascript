a84416ef00ca47c3c70d008f2d13e23c849f6b99
hbaughman <hbaughman741@gmail.com>
2014-09-17 13:24:09 -0400

    Create question function

--- jsis-quiz/quiz.js
+++ jsis-quiz/quiz.js
@@ -33,3 +33,49 @@ console.log(firstUser.showNameAndScores());
 secondUser = new User('Peter', 'peter@example.com');
 secondUser.saveScore(18);
 console.log(secondUser.showNameAndScores());
+
+function Question(theQuestion, theChoices, theCorrectAnswer) {
+  this.question = theQuestion;
+  this.choices = theChoices;
+  this.theCorrectAnswer = theCorrectAnswer;
+  this.userAnswer = '';
+
+  // private properties: these cannot be changed by instances
+  var newDate = new Date(),
+      QUIZ_CREATED_DATE = newDate.toLocalDateString();
+
+  // This is the only way to access QUIZ_CREATED_DATE.
+  // This is an example of a privilege method: it can access private properties,
+  // and itself be called publically.
+  this.getQuizDate = function() {
+    return QUIZ_CREATED_DATE;
+  };
+
+  // Confirm the quiz was created.
+  console.log('Quiz created on: ' + this.getQuizDate());
+
+  // Define the prototype methods that will be inherited.
+  Question.prototype.getCorrectAnswer = function() {
+    return this.correctAnswer;
+  };
+
+  Question.prototype.getUserAnswer = function() {
+    return this.userAnswer;
+  };
+
+  Question.prototype.displayQuesction = function() {
+    var questionToDisplay = '<div class="question">' + this.question
+                          + '</div><ul>',
+        choiceCounter = 0;
+
+    this.choices.forEach(function(eachChoice) {
+      questionToDisplay += '<li><input type="radio" name="choice" value="'
+                        +  choiceCounter + '">' + eachChoice + '</li>';
+      choiceCounter ++;
+    });
+    questionToDisplay += '</ul>';
+    console.log(questionToDisplay);
+
+  };
+
+}
